46caaaabfe710eb044b6452cff500bbbf2ef6c18
darkfos82 <darkfos82@gmail.com>
2024-06-02 02:23:53 +0300

    Update Car Repository

--- src/repository/car_repository.py
+++ src/repository/car_repository.py
@@ -1,14 +1,12 @@
 #System
-from typing import List, Union
-import asyncio
+from typing import Tuple, Sequence
 
 #Other libraries
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, update, delete
+from sqlalchemy import select, Row
 
 #Local
 from src.model.car import Car
-from src.config.database import db_obj
 from src.repository.general_sql_repository import GeneralSQLRepository
 
 
@@ -16,3 +14,30 @@ class CarRepository(GeneralSQLRepository):
 
     def __init__(self, session: AsyncSession, model=Car):
         super().__init__(session=session, model=model)
+
+    async def get_all_car_for_id_carrier(self, id_carrier: int) -> Sequence[Row[Tuple[Car]]]:
+        """
+        Receipt of all carrier vehicles
+        """
+
+        stmt = select(Car).where(Car.id_carrier == id_carrier)
+        car_for_carrier = await self.session.execute(stmt)
+        return car_for_carrier.fetchall()
+
+    async def get_all_working_car(self) -> Sequence[Row[Tuple[Car]]]:
+        """
+        Getting all working car
+        """
+
+        stmt = select(Car).where(Car.workload_car == True)
+        all_working_car = await self.session.execute(stmt)
+        return all_working_car.fetchall()
+
+    async def get_all_working_car_by_carrier(self, id_carrier: int) -> Sequence[Row[Tuple[Car]]]:
+        """
+        Getting all working car for carrier
+        """
+
+        stmt = select(Car).where(Car.id_carrier == id_carrier and Car.workload_car == True)
+        all_working_car_for_carrier = await self.session.execute(stmt)
+        return all_working_car_for_carrier.fetchall()
